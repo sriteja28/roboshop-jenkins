1ebcccbd0bbe428878f1c8802abf9bf0d18f9290
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-11 14:09:41 +0530

    Updating Jenkinsfile for project

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -2,17 +2,12 @@ def call() {
     node('workstation') {
 
         stage('Code Checkout') {
-//            sh 'env'
-//            sh 'find . | grep "^./" |xargs rm -rf'
-
-            if(env.TAG_NAME ==~ ".*") {
+            if (env.TAG_NAME ==~ ".*") {
                 env.gitbrname = "refs/tags/${env.TAG_NAME}"
             } else {
                 env.gitbrname = "${env.BRANCH_NAME}"
             }
             checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: "https://github.com/sriteja28/${env.component}"]], branches: [[name: gitbrname]]], poll: false
-
-            // git branch: env.BRANCH_NAME, url: 'https://github.com/sriteja28/frontend'
         }
 
         if (env.cibuild == "java") {
@@ -36,7 +31,7 @@ def call() {
 
         if (env.TAG_NAME ==~ ".*") {
             stage('Publish a Artifact') {
-                if (env.cibuild == "nginx"){
+                if (env.cibuild == "nginx") {
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
                 }
             }
