0eac67ede1afb51a212dd722bcbfe4791109fd44
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-11 12:37:27 +0530

    Updating Jenkinsfile for project

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,33 +1,29 @@
 def call() {
     node('workstation') {
 
-        stages {
-
-            if (env.cibuild == "java") {
-
-                stage('Build') {
-                    sh 'mvn package'
-                }
+        if (env.cibuild == "java") {
+            stage('Build') {
+                sh 'mvn package'
             }
+        }
 
-            stage('Unit tests') {
-                echo 'Unit tests'
-            }
+        stage('Unit tests') {
+            echo 'Unit tests'
+        }
 
-            stage('Code Analysis') {
-                echo 'sonar'
-                // sh 'sonar-scanner -Dsonar.host.url=http://172.31.81.241:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
-            }
+        stage('Code Analysis') {
+            echo 'sonar'
+            // sh 'sonar-scanner -Dsonar.host.url=http://172.31.81.241:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
+        }
 
-            stage('Security Scans') {
-                echo 'Security Scans'
-            }
+        stage('Security Scans') {
+            echo 'Security Scans'
+        }
 
-            stage('Publish a Artifact') {
-                echo 'Publish a Artifact'
-            }
-
+        stage('Publish a Artifact') {
+            echo 'Publish a Artifact'
         }
 
     }
+
 }
