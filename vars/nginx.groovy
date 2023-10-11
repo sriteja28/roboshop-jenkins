453e005d111e3ea6862fbcd805ba4274219b4074
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-11 12:22:43 +0530

    Updating Jenkinsfile for project

---
+++ vars/nginx.groovy
@@ -0,0 +1,38 @@
+def call() {
+    pipeline {
+
+        agent {
+            node { label 'workstation' }
+        }
+
+        stages {
+
+            stage('Unit tests') {
+                steps {
+                    echo 'Unit tests'
+                }
+            }
+
+            stage('Code Analysis') {
+                steps {
+                    echo 'sonar'
+                    // sh 'sonar-scanner -Dsonar.host.url=http://172.31.81.241:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
+                }
+            }
+
+            stage('Security Scans') {
+                steps {
+                    echo 'Security Scans'
+                }
+            }
+
+            stage('Publish a Artifact') {
+                steps {
+                    echo 'Publish a Artifact'
+                }
+            }
+
+        }
+
+    }
+}
